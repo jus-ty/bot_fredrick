7a52b2b08037a2b046ab718ee07fcbf8051ebccb
jus-ty <justinwalton48@gmail.com>
2022-08-24 16:23:07 +1000

    set up VPC via terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    description = "The AWS region to create the resource"
+}
+
+variable "vpc_cidr_block" {
+    description = "CIDR block for the VPC"
+}
+
+variable "vpc_tags" {
+    description = "Tags for the VPC"
+}
