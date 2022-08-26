e18180f0592289f976ad608855c9300f957757c6
jus-ty <justinwalton48@gmail.com>
2022-08-27 02:08:27 +1000

    comments for instance types

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "bastion_instance" {
   ami                       = var.instance_ami
-  instance_type             = "t3.large"
+  instance_type             = "t3.large" # Make it t2.micro once RDP Hop to private instance is figured out
   subnet_id                 = var.public_subnet_id
   vpc_security_group_ids    = [var.bastion_security_group_id]
   key_name                  = "bot_fredrick_kp"
@@ -9,7 +9,7 @@ resource "aws_instance" "bastion_instance" {
 
 resource "aws_instance" "private_instance" {
   ami                       = var.instance_ami
-  instance_type             = "t2.micro"
+  instance_type             = "t2.micro" # Change to best figured out instance type
   subnet_id                 = var.private_subnet_id
   vpc_security_group_ids    = [var.private_instance_security_group_id]
   key_name                  = "bot_fredrick_kp"
