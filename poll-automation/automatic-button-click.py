e572e8a9f2463610461442683e618009fcc08fcc
Andrew Guo <andrewguo21@gmail.com>
2022-08-28 18:02:21 +1000

    login_facebook function refactor using Selenium

--- poll-automation/automatic-button-click.py
+++ poll-automation/automatic-button-click.py
@@ -1,5 +1,6 @@
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 import pyautogui
 import time
@@ -25,13 +26,10 @@ def write_with_sleep(write_input: str, sleep_value: int):
     time.sleep(sleep_value)
 
 
-# Selenium always opens a fresh private browser. Ref: https://stackoverflow.com/questions/27630190/python-selenium-incognito-private-mode
-#driver = webdriver.Chrome(PATH)
-#driver.get('https://www.facebook.com/messages/t/5024734647638392/')
 
 try:
     options = webdriver.ChromeOptions()
-    options.add_argument("--incognito")
+    options.add_argument("--incognito")                 # Optional. Selenium always opens a fresh private browser. Ref: https://stackoverflow.com/questions/27630190/python-selenium-incognito-private-mode
     options.add_experimental_option("detach", True)     # https://stackoverflow.com/questions/51865300/python-selenium-keep-browser-open
     driver = webdriver.Chrome(DRIVERPATH, options=options)
 
@@ -46,7 +44,7 @@ def open_browser(driver, url, browserType = "chrome"):
 
     if browserType == "chrome":
         try:
-            driver.get(url)
+            driver.get(url)                             #Use for testing, driver.get('https://www.facebook.com/messages/t/5024734647638392/')
             time.sleep(2)
 
         except Exception as e:
@@ -58,21 +56,25 @@ def login_facebook(driver, EMAIL, PASSWORD):
     Log into facebook messenger
     """
 
-    #browser.find_element(By.NAME, 'username').send_keys('MYEMAIL', Keys.TAB, 'MYPW', Keys.ENTER)
+    driver.find_element(By.NAME, 'email').send_keys(f'{EMAIL}', Keys.TAB, f'{PASSWORD}', Keys.ENTER)                    # HTML, name="email"
+    time.sleep(7)
 
+    """
     write_with_sleep(f'{EMAIL}', 1) 
     press_with_sleep('tab', 1)
     write_with_sleep(f'{PASSWORD}', 1) 
     press_with_sleep('tab', 1)
     press_with_sleep('enter', 10)
     time.sleep(2)
+    """
+    
     
     
 def create_group_messenger_poll(driver, pollTitle, option1, option2):
     """
     Create the poll
     """
-    
+
     # Selenium locating elements. Ref: https://selenium-python.readthedocs.io/locating-elements.html
     # https://stackoverflow.com/questions/55980282/how-to-find-and-click-on-a-button-element-that-contains-multiple-classes
     openMoreActionsClass = "qi72231t.o9w3sbdw.nu7423ey.tav9wjvu.flwp5yud.tghlliq5.gkg15gwv.s9ok87oh.s9ljgwtm.lxqftegz.bf1zulr9.frfouenu.bonavkto.djs4p424.r7bn319e.bdao358l.fsf7x5fv.tgm57n0e.jez8cy9q.s5oniofx.dnr7xe2t.aeinzg81.om3e55n1.cr00lzj9.rn8ck1ys.s3jn8y49.g4tp4svg.o9erhkwx.dzqi5evh.hupbnkgi.hvb2xoa8.fxk3tzhb.jl2a5g8c.f14ij5to.l3ldwz01.icdlwmnq.d2hqwtrz.fzd7ma4j.o9wcebwi.lcfup58g.i0rxk2l3.laatuukc.b7mnygb8.iec8yc8l"
@@ -91,6 +93,6 @@ def create_group_messenger_poll(driver, pollTitle, option1, option2):
     press_with_sleep('enter', 1)
 
 
-open_browser(driver, f'https://www.facebook.com/messages/t/{THREAD_ID}/')
+open_browser(driver, f'https://www.facebook.com/messages/t/{THREAD_ID}/', 'chrome')
 login_facebook(driver, f'{EMAIL}', f'{PASSWORD}')
 create_group_messenger_poll(driver, '[AG] testing', 'option1', 'option2')
