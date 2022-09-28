a30aa781524ffc8ac56e473926003ead565b9000
jus-ty <justinwalton48@gmail.com>
2022-09-28 11:03:42 +1000

    memory increase

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -4,13 +4,13 @@ resource "aws_lambda_function" "lambda" {
   runtime               = "python3.8"
   handler               = "main.lambda_handler" # python_script_name.driver_function_name
   timeout               = 180
+  memory_size           = 1048
   filename              = "${data.archive_file.lambda_zip_file.output_path}"
   source_code_hash      = "${data.archive_file.lambda_zip_file.output_base64sha256}"
   tags                  = var.lambda_name_tags
-  # layers                = [
-  #                         "arn:aws:lambda:ap-southeast-2:537519792485:layer:bot_fredrick_python_packages:2",
-  #                         "arn:aws:lambda:ap-southeast-2:537519792485:layer:chromedriver_linux:2"
-  #                         ]
+  layers                = [
+                          "arn:aws:lambda:ap-southeast-2:537519792485:layer:HeadlessChromium:2"
+                          ]
 
   vpc_config {
     subnet_ids          = [var.lambda_subnet_id]
